cf4a1439da896fcef5b3e1f29d18e7002305f3f2
seankang426 <skang@stcharlessd.org>
2017-11-06 09:57:53 -0600

    finished work

--- Lab 03 - Create a Quiz/main_program.py
+++ Lab 03 - Create a Quiz/main_program.py
@@ -1,1 +1,31 @@
-#
+q1 = int(input('What is 2 + 2? '))
+if q1 == 4:
+    print('Correct!')
+else:
+    print('Wrong! The answer is 4')
+q2 = input('Who is the the best starter pokemon? ')
+if q2 == ('Squirtle'):
+    print('Correct!')
+else:
+    print('Wrong! The answer is Squirtle')
+q3 = input('What is spongebobs pet snail named? ')
+if q3 == ('Gary'):
+    print('Correct!')
+else:
+    print('Wrong! The answer is Gary')
+q4 = int(input('How much wood could a wood chuck chuck if a wood chuck could chuck wood?'))
+if q4 == 700:
+    print('Correct')
+else:
+    print('Wrong! The answer is 700')
+q5 = input('How awesome is Chuck Norris')
+if q5 == ('Yes'):
+    print('Correct')
+else:
+    print('Wrong! The answer is Yes')
+
+
+          
+          
+
+        
--- Lab 04 - Camel/main_program.py
+++ Lab 04 - Camel/main_program.py
@@ -1,1 +1,86 @@
-#
+import random
+print('Welcome to Camel!')
+print('You have stolen a camel to make your way across the great Mobi desert.')
+print('The natives want their camel back and are chasing you down! Survive your')
+print('desert trek and outrun the natives.')
+done = False
+milestraveled = 0
+thirst = 0
+cameltiredness = 0
+natives = -20
+canteen = 3
+
+while done == False:
+    print('A. Drink from your canteen.')
+    print('B. Ahead moderate speed.')
+    print('C. Ahead full speed.')
+    print('D. Stop for the night.')
+    print('E. Status check.')
+    print('Q. Quit.')
+    userchoice = input('Your choice? ').upper()
+    if userchoice == ('Q'):
+        done = True
+        
+    elif userchoice == ('E'):
+        print('Miles traveled:',milestraveled)
+        print('Drinks in canteen',thirst)
+        print('The natives are',natives,'miles behind you')
+    elif userchoice == ('D'):
+        cameltiredness = 0
+        print('Your camel is happy.')
+        n1 = random.randint(7,14)
+        natives = natives + n1
+    elif userchoice == ('C'):
+        n2 = random.randint(10,20)
+        milestraveled = milestraveled + n2
+        print(milestraveled, 'Miles traveled')
+        thirst + 1
+        n3 = random.randint(1,3)
+        cameltiredness = cameltiredness + n3
+        n1 = random.randint(7,14)
+        natives = natives + n1
+    elif userchoice == ('B'):
+        n2 = random.randint(5,12)
+        milestraveled = milestraveled + n2
+        print(milestraveled)
+        thirst + 1
+        cameltiredness + 1
+        n1 = random.randint(7,14)
+        natives = natives + n1
+    elif userchoice == ('A'):
+        if canteen > 0:
+            canteen = canteen - 1
+        else:
+            print('You dont have enough water.')
+    if thirst > 6:
+        print('You died of thirst!')
+        done = True
+    elif thirst > 4:
+        print('You are thirsty!')
+        
+
+    elif cameltiredness > 5:
+        print('Your camel is dead :(')
+        done = True
+    elif cameltiredness >= 8:
+        print('Your camel is getting tired.')
+
+    if abs(natives - milestraveled) <= 15:
+        print('The natives are getting close!')
+    chance = random.randint(1,20)
+    if chance == 5:
+        canteen = o
+        thirst = 0
+        cameltiredness = 0
+        print('You found an oasis')
+
+    if milestraveled >= 200 and done == False:
+        print('You made it across the Mobi Desert!') 
+        
+
+        
+        
+        
+    
+        
+              
